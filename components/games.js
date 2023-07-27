3fa85d77d9a0a5ca23125fc961e0c6e56dc33d67
eisa <eisabacha@gmail.com>
2023-07-27 19:21:24 +0200

    27_7

---
+++ components/games.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Games = () => {
+  return (
+    <div>games
+
+<div><script src="https://cdn.htmlgames.com/embed.js?game=AirportSniper&amp;bgcolor=white"></script></div>
+    </div>
+  )
+}
+
+export default Games
--- pages/preise.js
+++ pages/preise.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Games from '@/components/games'
 
 const PREISE = () => {
   return (
     <div>
+      <Games/>
         <h1>
         I am the PREISE page
         </h1>
